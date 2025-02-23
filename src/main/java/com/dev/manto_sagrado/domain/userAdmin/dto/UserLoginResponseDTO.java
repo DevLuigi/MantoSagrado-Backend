a0f063c20e0ff054be96be4e685cd2463b5713ff
DevLuigi <luigidasilvacoelho@gmail.com>
2025-02-23 16:40:39 -0300

    feat: Adiciona update de usuários admin

--- src/main/java/com/dev/manto_sagrado/domain/userAdmin/dto/UserLoginResponseDTO.java
+++ src/main/java/com/dev/manto_sagrado/domain/userAdmin/dto/UserLoginResponseDTO.java
@@ -1,5 +1,6 @@
 package com.dev.manto_sagrado.domain.userAdmin.dto;
 
+import com.dev.manto_sagrado.domain.userAdmin.Enum.Group;
 import com.dev.manto_sagrado.domain.userAdmin.entity.UserAdmin;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,8 @@ public class UserLoginResponseDTO {
     private String name;
     private String email;
     private String password;
+    private Group userGroup;
+    private String cpf;
 
     public static UserLoginResponseDTO fromUserAdmin(UserAdmin user) {
         return UserLoginResponseDTO.builder()
@@ -18,6 +21,8 @@ public class UserLoginResponseDTO {
                 .name(user.getName())
                 .email(user.getEmail())
                 .password(user.getPassword())
+                .userGroup(user.getUserGroup())
+                .cpf(user.getCpf())
                 .build();
     }
 }
--- src/main/java/com/dev/manto_sagrado/domain/userAdmin/entity/UserAdmin.java
+++ src/main/java/com/dev/manto_sagrado/domain/userAdmin/entity/UserAdmin.java
@@ -1,8 +1,10 @@
 package com.dev.manto_sagrado.domain.userAdmin.entity;
 
+import com.dev.manto_sagrado.domain.userAdmin.Enum.Group;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Table(name = "users")
@@ -16,12 +18,19 @@ public class UserAdmin {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank(message = "Nome deve ser preenchido")
+    @NotNull(message = "Nome deve ser preenchido")
     private String name;
 
-    @Email @NotBlank(message = "Email deve ser preenchido")
+    @Email @NotNull(message = "Email deve ser preenchido")
     private String email;
 
-    @NotBlank
+    @NotNull
     private String password;
+
+    @NotNull
+    @Enumerated(EnumType.ORDINAL)
+    private Group userGroup;
+
+    @NotNull
+    private String cpf;
 }
